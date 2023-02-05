d6501db4ba9552b364c943e7d45a5b44d536390a
TheBidus <123047080+TheBidus@users.noreply.github.com>
2023-02-05 02:11:39 -0500

    set number

--- World.gd
+++ World.gd
@@ -10,7 +10,7 @@ var empty_spots_index = []
 var number_of_colors = 2
 var ratio_empty_spots = 0.2
 var acceptance_ratio = 50
-var ratio_of_meeple = [0.4, 0.4, 0, 0, 0, 0, 0, 0]
+var ratio_of_meeple = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
 
 onready var meeple_position_x = 32
 onready var meeple_position_y = 32
